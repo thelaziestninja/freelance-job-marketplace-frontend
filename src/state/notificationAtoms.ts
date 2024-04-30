1ee7f166312db83d5adffd2dad9b3b4080e33a16
Godji Berry <verginia.razvigorova@gmail.com>
2024-04-30 16:43:10 +0300

    removed unnecessary atoms

--- src/state/notificationAtoms.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "jotai";
-
-export const notificationAtom = atom<string | null>(null);
-
-export const setMessageAtom = atom(null, (get, set, message: string) => {
-  set(notificationAtom, message);
-});
